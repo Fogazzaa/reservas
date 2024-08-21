4e4f8404ee58ed0ce825cfb3bd9722f2fe793dd4
mariajuliacintra <mariajuliacintra31@gmail.com>
2024-08-21 15:17:00 -0300

    21-08

---
+++ back-end/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express"); //importando o módolo express
+
+//Define uma classe para organizar a lógica da aplicação
+class AppController {
+  constructor() {
+    //Cria uma nova instância
+    //criar a instância do Express dentro da classe
+    this.express = express();
+    //Chama o método middlewares para configurar os middlewares
+    this.middlewares();
+    //Chama o método routes para definir as rotas da Api
+    this.routes();
+  }
+  middlewares() {
+    //Permitir que a aplicação receba dados em formato JSON nas requisições(solicitação)
+    this.express.use(express.json());
+  }
+  //Define as rotas da nossa Api
+  routes() {
+    //Define uma rota GET para o caminho health
+    this.express.get("/health/", (req, res) => {
+      res.send({ status: "OK" });
+    }); //Essa rota é usada para verificar se a Api está OK
+  }
+}
+//Exportândo a instância de express configurada, para que seja acessada em outros arquivos
+module.exports = new AppController().express;
---
+++ back-end/src/server.js
@@ -0,0 +1,3 @@
+const app = require("./index");
+//Inicia o servidor na porta 5000, neste caso como está local a Api será acessível em http://localhost:5000/
+app.listen(5000);
