3f9bc553f80c56ebbe4f459ce74625290d6414f6
fogazza <vinicius.cintra2801@gmail.com>
2024-10-15 13:10:25 -0300

    15-10-24

--- main/front-end/app.js
+++ main/front-end/app.js
@@ -40,7 +40,7 @@ document
         console.log("Usuário Cadastrado! ");
 
         // limpa os campos do formulario, após o sucesso do cadastro
-        document.getElementById("formulario-cadastro").reset();
+        window.location.href="principal.html";
       }) // --- fechamento 'data'
       //captura qualquer erro que ocorra durante o processo de requisição/ resposta
       .catch((error) => {
@@ -49,43 +49,43 @@ document
         console.error("Erro:", error.message);
       }); // --- fechamento 'catch(error)'
 
-      fetch("http://localhost:5000/reservas/v1/usuario", {
-        // realiza uma chamada HTTP para o servidor (a rota definida)
-        method: "GET",
-        headers: {
-          // a requisição será em formato JSON
-          "Content-Type": "application/json",
-        },
-        // transforma os dados do formulário em uma string json para serem enviados no corpo da requisição
-        body: JSON.stringify({ nome_usuario, NIF, senha, email }),
-      })
-        .then((response) => {
-          // tratamento da resposta do servidor / api
-          if (response.ok) {
-            // verifica se a resposta foi bem-sucedida (status: 20*)
-            return response.json();
-          } // --- fechamento 'response.ok'
-          // convertendo o erro em formato JSON
-          return response.json().then((err) => {
-            // mensagem retornada do servidor, acessa pela chave 'error'
-            throw new Error(err.error);
-          }); // --- fechamento 'response error'
-        }) // --- fechamento 'response'
-        .then((data) => {
-          // executa a resposta de sucesso  - retorna ao usuario final
-          // exibe alerta com o nome do usuario com o nome que acabou de ser cadastrado (front)
-          alert("Usuário cadastrado com sucesso!");
-          // exibe o log no terminal
-          console.log("Usuário Cadastrado! ");
+    //   fetch("http://localhost:5000/reservas/v1/usuario", {
+    //     // realiza uma chamada HTTP para o servidor (a rota definida)
+    //     method: "GET",
+    //     headers: {
+    //       // a requisição será em formato JSON
+    //       "Content-Type": "application/json",
+    //     },
+    //     // transforma os dados do formulário em uma string json para serem enviados no corpo da requisição
+    //     body: JSON.stringify({ nome_usuario, NIF, senha, email }),
+    //   })
+    //     .then((response) => {
+    //       // tratamento da resposta do servidor / api
+    //       if (response.ok) {
+    //         // verifica se a resposta foi bem-sucedida (status: 20*)
+    //         return response.json();
+    //       } // --- fechamento 'response.ok'
+    //       // convertendo o erro em formato JSON
+    //       return response.json().then((err) => {
+    //         // mensagem retornada do servidor, acessa pela chave 'error'
+    //         throw new Error(err.error);
+    //       }); // --- fechamento 'response error'
+    //     }) // --- fechamento 'response'
+    //     .then((data) => {
+    //       // executa a resposta de sucesso  - retorna ao usuario final
+    //       // exibe alerta com o nome do usuario com o nome que acabou de ser cadastrado (front)
+    //       alert("Usuário cadastrado com sucesso!");
+    //       // exibe o log no terminal
+    //       console.log("Usuário Cadastrado! ");
   
-          // limpa os campos do formulario, após o sucesso do cadastro
-          document.getElementById("formulario-cadastro").reset();
-        }) // --- fechamento 'data'
-        //captura qualquer erro que ocorra durante o processo de requisição/ resposta
-        .catch((error) => {
-          // exibe alerta no (front) com erro processado
-          alert("Erro no cadastro: " + error.message);
-          console.error("Erro:", error.message);
-        }); // --- fechamento 'catch(error)'
+    //       // limpa os campos do formulario, após o sucesso do cadastro
+    //       window.location.href="principal.html";
+    //     }) // --- fechamento 'data'
+    //     //captura qualquer erro que ocorra durante o processo de requisição/ resposta
+    //     .catch((error) => {
+    //       // exibe alerta no (front) com erro processado
+    //       alert("Erro no cadastro: " + error.message);
+    //       console.error("Erro:", error.message);
+    //     }); // --- fechamento 'catch(error)'
 
   });
