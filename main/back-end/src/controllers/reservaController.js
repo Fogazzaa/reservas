876d027dc6526b9a57d0c10d4d8e8d571a136ccd
fogazza <vinicius.cintra2801@gmail.com>
2024-11-15 13:55:45 -0300

    13-11-24

--- main/back-end/src/controllers/reservaController.js
+++ main/back-end/src/controllers/reservaController.js
@@ -3,23 +3,18 @@ const connect = require("../db/connect"); // Importa o módulo de conexão com o
 module.exports = class AgendamentoController {
   static async createReserva(req, res) {
     // Extrai os dados do corpo da requisição
-    const { fk_id_usuario, fk_id_sala, datahora_inicio, datahora_fim } = req.body;
+    const { fk_id_usuario, fk_id_sala, datahora_inicio, datahora_fim } =
+      req.body;
 
     // Valida se todos os campos obrigatórios estão preenchidos
-    if (
-      !fk_id_usuario ||
-      !fk_id_sala ||
-      !datahora_inicio ||
-      !datahora_fim
-    ) {
+    if (!fk_id_usuario || !fk_id_sala || !datahora_inicio || !datahora_fim) {
       return res
         .status(400)
         .json({ error: "Todos os campos devem ser preenchidos" });
     }
 
     // A consulta SQL agora verifica se já existe uma reserva que se sobreponha
-    const queryHorario =
-      `SELECT datahora_inicio, datahora_fim FROM reserva WHERE fk_id_sala = ? AND (
+    const queryHorario = `SELECT datahora_inicio, datahora_fim FROM reserva WHERE fk_id_sala = ? AND (
         (datahora_inicio < ? AND datahora_fim > ?) OR  -- Novo horário começa antes e termina depois da reserva existente
         (datahora_inicio < ? AND datahora_fim > ?) OR  -- Novo horário começa antes e termina depois da reserva existente
         (datahora_inicio >= ? AND datahora_inicio < ?) OR  -- Novo horário começa dentro de um horário já reservado
@@ -35,14 +30,22 @@ module.exports = class AgendamentoController {
       datahora_inicio,
       datahora_fim,
       datahora_inicio,
-      datahora_fim
+      datahora_fim,
     ];
 
     try {
+      const limiteHora = 60 * 60 * 1000; // 1 hora em milissegundos
+
       // Verifica se já existe alguma reserva conflitante no horário
       const [resultadosH] = await connect
         .promise()
         .query(queryHorario, valuesHorario);
+
+      if (new Date(datahora_fim) - new Date(datahora_inicio) > limiteHora) {
+        return res
+          .status(400)
+          .json({ error: "O tempo de reserva excede o limite (1h)" });
+      }
 
       // Se houver qualquer reserva que se sobreponha
       if (resultadosH.length > 0) {
@@ -58,7 +61,7 @@ module.exports = class AgendamentoController {
         fk_id_usuario,
         fk_id_sala,
         datahora_inicio,
-        datahora_fim
+        datahora_fim,
       ];
 
       // Realiza o agendamento (inserção)
