f2782786af7cd6accd2b6b3350c03279800f0861
mariajuliacintra <mariajuliacintra31@gmail.com>
2024-12-02 09:01:51 -0300

    02-12-24

--- main/back-end/src/db/connect.js
+++ main/back-end/src/db/connect.js
@@ -4,13 +4,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
     connectionLimit:10,
     host:'10.89.240.84',
-<<<<<<< HEAD
     user:'MariaJulia',
-=======
-    user:'ViniciusFogaca',
->>>>>>> 3865bb7e431d6282a2b9b118ff7e9d3b6c841df4
-    password:'1234',
-    database:'rs'
+    password: '1234'
 })
 
 module.exports = pool;
