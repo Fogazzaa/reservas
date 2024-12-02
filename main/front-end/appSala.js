4c92da2602fcda6322239e3e3ab56559aeb15e96
MariaClaraOliveira08 <mahh.oliveeira16@gmail.com>
2024-12-02 13:10:58 -0300

    02-12

--- main/front-end/appSala.js
+++ main/front-end/appSala.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", getAllSalasTabelaFiltroNome);
 
 // Função que busca as informações das salas e preenche a tabela.
 function getAllSalasTabela() {
-  fetch("http://localhost:5000/reservas/v1/sala", {
+  fetch("http://10.89.240.84:5000/reservas/v1/sala", {
     method: "GET",
     headers: {
       "Content-Type": "application/JSON",
@@ -67,14 +67,29 @@ function getAllSalasTabela() {
 
 function getAllSalasTabelaFiltroData() {
   // Realiza uma requisição GET para a URL
+  // Obtém o valor do campo de entrada de data com o ID "filtro-data"
+  // e armazena como uma string na variável data_inicio_string.
   const data_inicio_string = document.getElementById("filtro-data").value;
+
+  // Cria um objeto Date a partir da string de data de início.
+  // Isso converte a string em um formato que pode ser manipulado como uma data.
   const data_inicio = new Date(data_inicio_string);
+
+  // Obtém novamente o valor do campo de entrada de data com o ID "filtro-data"
+  // e armazena como uma string na variável data_fim_string.
+  // (Nota: Isso pode ser desnecessário, já que estamos usando o mesmo campo de entrada.)
   const data_fim_string = document.getElementById("filtro-data").value;
+
+  // Cria um objeto Date a partir da string de data de fim.
+  // Inicialmente, data_fim será igual a data_inicio, pois estamos usando o mesmo valor.
   const data_fim = new Date(data_fim_string);
+
+  // Ajusta a data de fim para ser um dia após a data de início.
+  // O método setDate() altera o dia do mês da data_fim.
   data_fim.setDate(data_inicio.getDate() + 1);
 
   console.log(`Filtrar por data: ${data_inicio} - ${data_fim}`);
-  fetch("http://localhost:5000/reservas/v1/sala", {
+  fetch("http://10.89.240.84:5000/reservas/v1/sala", {
     method: "GET",
     headers: {
       "Content-Type": "application/JSON",
@@ -94,13 +109,13 @@ function getAllSalasTabelaFiltroData() {
 
     // Após obter os dados em JSON, preenche a tabela
     .then((data) => {
-      const salaLista = document.getElementById("sala-list-tabela"); // Obtém o elemento da tabela onde os dados das salas estão
+      const salaLista = document.getElementById("sala-list-tabela"); //Obtém o elemento da tabela onde os dados das salas estão
       salaLista.innerHTML = ""; // Limpa a lista antes de adicionar novos itens
 
       // Verifica se há salas retornadas e as adiciona à tabela
       data.salas.forEach((sala) => {
         // Cria uma nova linha da tabela
-        const tr = document.createElement("tr");
+        const tr = document.createElement("tr"); // Cria uma nova linha da tabela
 
         // Cria a célula para o nome da sala
         const tdNome = document.createElement("td");
@@ -148,7 +163,7 @@ function getAllSalasTabelaFiltroNome() {
   const nome = document.getElementById("filtro-nome").value;
 
   console.log(`Nome da sala: ${nome}`);
-  fetch("http://localhost:5000/reservas/v1/sala", {
+  fetch("http://10.89.240.84:5000/reservas/v1/sala", {
     method: "GET",
     headers: {
       "Content-Type": "application/JSON",
