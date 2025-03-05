87f837b06af29040d8ad7b8f6369614ad5b1d84d
fogazza <vinicius.cintra2801@gmail.com>
2025-03-05 09:42:42 -0300

    05-03-25 / rearrumação das rotas

--- main/front-end/telaCadastro/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Cadastro from './Cadastro/Cadastro'
-
-function App() {
-  return <div><Cadastro/></div>;
-}
-
-export default App;
--- main/front-end/telaLogin/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Login from './Login/Login'
-
-function App() {
-  return <div><Login/></div>;
-}
-
-export default App;
